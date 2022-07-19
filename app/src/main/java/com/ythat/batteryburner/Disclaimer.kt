1f19126f8b6a18b3b3d4f7d97000da3d0e860ecb
Yohann <Yoh@macbook.local>
2022-07-19 13:17:49 -0400

    Disclaimer UI tweaks

--- app/src/main/java/com/ythat/batteryburner/Disclaimer.kt
+++ app/src/main/java/com/ythat/batteryburner/Disclaimer.kt
@@ -1,6 +1,5 @@
 package com.ythat.batteryburner
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -8,7 +7,6 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,8 +15,8 @@ import com.ythat.batteryburner.ui.theme.BatteryBurnerTheme
 @Composable
 fun DisclaimerScreen(onDisclaimerDismiss: () -> Unit = {}, modifier: Modifier = Modifier) {
 
-    Surface(shape = MaterialTheme.shapes.small, modifier = modifier.padding(4.dp)) {
-        Column(modifier = modifier) {
+    Surface(shape = MaterialTheme.shapes.small, modifier = modifier) {
+        Column(modifier = modifier.padding(4.dp)) {
             IconButton(onClick = onDisclaimerDismiss) {
                 Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
                     Text(text = "Warning", modifier = modifier.align(Alignment.CenterVertically))
