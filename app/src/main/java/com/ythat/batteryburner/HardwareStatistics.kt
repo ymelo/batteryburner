04edb815fe42687c25bb91994ed5f45331af8453
Yohann <Yoh@macbook.local>
2022-07-19 13:18:18 -0400

    Added some fake CPU load for testing

--- app/src/main/java/com/ythat/batteryburner/HardwareStatistics.kt
+++ app/src/main/java/com/ythat/batteryburner/HardwareStatistics.kt
@@ -1,11 +1,50 @@
 package com.ythat.batteryburner
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import kotlin.math.pow
+
 
 data class HardwareStatistics(val cpuName: String, val coreCount: Int)
 
-class HardwareStatisticsProvider() {
+class HardwareStatisticsRepository() {
     private var processNames = listOf("/system/bin/cat", "/proc/cpuinfo")
     fun hardwareInfo() : HardwareStatistics {
-        return HardwareStatistics("test", Runtime.getRuntime().availableProcessors())
+        val processBuilder = ProcessBuilder(processNames)
+//        processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE)
+        return HardwareStatistics("Unknown", Runtime.getRuntime().availableProcessors())
     }
+}
+
+class DrainageViewModel: ViewModel() {
+    private var currentJob: Job? = null
+    val keepAwake: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>().also { it.value = false }
+    }
+
+    fun makeCpuBurn(concurrentThreads: Int = 0) {
+        currentJob?.cancel()
+        Log.d("job", "Current job $currentJob")
+        if (concurrentThreads > 0) {
+            currentJob = viewModelScope.launch {
+                burn()
+            }
+        }
+
+    }
+
+    private suspend fun burn() {
+        withContext(Dispatchers.IO) {
+            for(i in 0..100) {
+                ensureActive()
+                delay(100)
+                println("POWA ${i.toDouble().pow(i.toDouble())}")
+//                i.toDouble().pow(i.toDouble())
+            }
+        }
+    }
+
 }
