c60ef77d278731b437d38f7f288e24e6330a5974
Yohann <Yoh@macbook.local>
2022-07-26 10:14:37 -0400

    CPU loading done per thread
    This allows an easier control of how much stress we put on the CPU

--- app/src/main/java/com/ythat/batteryburner/DrainageViewModel.kt
+++ app/src/main/java/com/ythat/batteryburner/DrainageViewModel.kt
@@ -1,21 +1,24 @@
 package com.ythat.batteryburner
 
 import android.app.Application
-import android.hardware.Camera
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraManager
+import android.os.HardwarePropertiesManager
 import android.util.Log
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ythat.batteryburner.data.HardwareRepository
 import kotlinx.coroutines.*
 import kotlin.math.pow
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 
 data class HardwareStatistics(val cpuName: String, val coreCount: Int)
 
 class DrainageViewModel(application: Application): AndroidViewModel(application) {
     private val hardwareRepository = HardwareRepository()
-    private var currentJob: Job? = null
+    private var currentJobs: MutableList<Job> = mutableListOf()
 
     private val keepAwake: MutableLiveData<Boolean> by lazy {
         MutableLiveData<Boolean>().also { it.value = false }
@@ -28,12 +31,15 @@ class DrainageViewModel(application: Application): AndroidViewModel(application)
     }
 
     fun makeCpuBurn(concurrentThreads: Int = 0) {
-        currentJob?.cancel()
-        Log.d("job", "Current job $currentJob")
-        if (concurrentThreads > 0) {
-            currentJob = viewModelScope.launch(CoroutineName("Yoplay")) {
+        for(job in currentJobs) {
+            job.cancel()
+        }
+        Log.d("job", "Current job $currentJobs")
+        for(i in 1..concurrentThreads) {
+            Log.d("CPU", "Starting on new single thread $i")
+            currentJobs.add(viewModelScope.launch(context = newSingleThreadContext("test$i")) {
                 burn()
-            }
+            })
         }
     }
 
@@ -44,8 +50,8 @@ class DrainageViewModel(application: Application): AndroidViewModel(application)
     private suspend fun burn() {
         withContext(Dispatchers.IO) {
             while(isActive) {
-                val i = Math.random()
-                println("POWA ${i.toDouble().pow(i.toDouble())}")
+                val i = Math.random() * 10000
+                i.pow(i)
             }
         }
     }
@@ -53,6 +59,5 @@ class DrainageViewModel(application: Application): AndroidViewModel(application)
     fun flashlight(enabled: Boolean) {
         val cw = CameraWrapper()
         cw.flashlight(enabled, getApplication<Application>().applicationContext)
-
     }
 }
